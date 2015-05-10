ddd417fe2a53312fcc1addeee9df9a92e48ef691
jannekai <janne.kaistinen@iki.fi>
2015-05-10 21:32:59 +0300

    Generate a response

--- plugin/generator.py
+++ plugin/generator.py
@@ -15,8 +15,8 @@ class Result:
 	def add_enum(self, descriptor, namespace):
 		self.enums.append(Enum(descriptor, namespace))
 
-	def add_message(descriptor):
-		self.messages.append(Message(desc, namespace))
+	def add_message(self, descriptor, namespace):
+		self.messages.append(Message(descriptor, namespace))
 
 class Enum:
 	def __init__(self, desc, namespace):
@@ -51,26 +51,34 @@ class Message:
 #
 # Process files
 #
-def process_file(fdesc):
-	result = Result()
+def process_file(fdesc, result):
 	namespace = fdesc.package
-
 	process_messages(fdesc.message_type, result, namespace)
 	process_enums(fdesc.enum_type, result, namespace)
-
-	dbg(enums)
-	dbg(messages)
 
 def process_messages(descriptors, result, namespace):
 	for desc in descriptors:
 		if not desc.options.map_entry:
 			result.add_message(desc, namespace)
-			process_messages(desc.nested_type,  namespace, messages, enums, desc.nested_type)
-			process_enums(namespace, enums, desc.enum_type)
+			process_messages(desc.nested_type, result, namespace)
+			process_enums(desc.enum_type, result, namespace)
 
 def process_enums(descriptors, result, namespace):
 	for desc in descriptors:
-		enums.append(Enum(desc))
+		result.add_enum(desc, namespace)
+
+#
+# Create result
+#
+def generate_response(result):
+	response = plugin_pb2.CodeGeneratorResponse()
+	f = response.file.add()
+	f.name = "protobuf_generated.ex"
+
+	for enum in result.enums:
+		dbg(enum)
+
+	return response
 
 #
 # Helpers
@@ -87,12 +95,16 @@ def to_str(msg):
 def main():
 	data = sys.stdin.read()
 	request = plugin_pb2.CodeGeneratorRequest.FromString(data)
-	response = plugin_pb2.CodeGeneratorResponse()
 
+	result = Result()
 	for filename in request.file_to_generate:
 		for fdesc in request.proto_file:
 			if filename == fdesc.name:
-				process_file(fdesc)
+				process_file(fdesc, result)
+
+	# Generated file
+	response = generate_response(result)
+	sys.stdout.write(response.SerializeToString())
 
 if __name__ == '__main__':
 	main()
